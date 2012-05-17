c011498ff033ef3aafbc809b8cd2c543d17bee69
Alan Sherek <albenturkey@fullsail.edu>
2012-05-16 21:14:59 -0500

    ajax call

--- ASD/App/asdcouch/asdcouch/app/views/customers/map.js
+++ ASD/App/asdcouch/asdcouch/app/views/customers/map.js
@@ -1,8 +1,8 @@
 function (doc){
 	if(doc._id.substr(0,5) === "order"){
 		emit(doc._id.substr(5), {
-			"First Name" : doc.fname[1],
-			"Last Name"  : doc.lname[1]
+			"First" : doc.fname[1],
+			"Last"  : doc.lname[1]
 		});
 	}
 };
