6883400133f65522c774dae33232a6d76e9a9d98
Alan Sherek <albenturkey@fullsail.edu>
2012-05-17 13:00:04 -0500

    project 2 and 3

---
+++ ASD/App/asdcouch/asdcouch/app/_attachments/main.js
@@ -0,0 +1,108 @@
+$(document).ready(function(){
+	var cust = function(){
+		$.ajax({
+			"url":"_view/customers",
+			"dataType":"json",
+			"success":function(data){
+				$.each(data.rows, function(index, customer){
+					var fname = customer.value.First
+					var lname = customer.value.Last
+					$('#customerlist').append(
+						$('<li>').append(
+							$('<a>').attr("href","#")
+								.text(fname+" " + lname)
+						)
+					);
+				});
+				$('#customerlist').listview('refresh');
+				$('#customerButton').hide();
+				}
+			});
+	}
+	$('#customerButton').on('click',cust)
+
+			
+//  project 2 make up
+//xml
+
+	var parseXML = function(xml){
+		$(xml).find("order").each(function(){
+			var orderList = {};
+			orderList.fname = $(this).find("fname").text();
+			orderList.lname = $(this).find("lname").text();
+			console.log(orderList);
+			
+			$('#xmlData').after(' ' +
+			'<li class="ui-li-static ui-body-a">'+ 
+			'<p> First Name: '+
+			orderList.fname +
+			'<br>'+
+			'Last Name: '+
+			orderList.lname + 
+			'</p></li>');
+		
+		});
+	};
+			
+	$('#xmlData').on("click", function(){
+		$.ajax({
+			type: "GET",
+			url: "data.xml",
+			dataType: "xml",
+			success: parseXML
+		});
+	});
+	
+	// csv
+/*	$('#csvData').on("click", function(){
+		$.ajax({
+			url:'data.csv',
+			type:'GET',
+			dataType: 'text',
+			success: function(csvData){
+				console.log(csvData)
+				var items = csvData.split("\n");
+				for(var j=1; j<items.length;j++){
+					var row = items[j];
+					var columns = row.split(",");
+					$('#csvData').after('  ' + 
+	                '<li class="ui-li ui-li-static ui-body-a">' +
+					'<p>First Name: ' + columns[1] + '<br>' +	
+					'Last Name: ' + columns[2] + '<br>' +
+					'Number: ' + column[3] + '<br>' +  
+					'Size: ' + columns[4] + '<br>'+
+					'</p></li>');
+				}
+			}
+		});
+	});
+*/
+	$('#csvData').on("click", function(){
+	$.ajax({
+		url : 'data.csv',
+		type: 'GET',
+		dataType : 'text',
+		success: function(csvData){
+			console.log("This is my CSV: " , csvData);
+			var items = csvData.split("\n");
+			for(var j=1; j<items.length;j++){
+				var row = items[j];
+				var columns = row.split(",");
+				console.log('CSV is: ',columns)
+				$('#csvData').after('' + 
+                '<li class="ui-li ui-li-static ui-body-a">' +
+				'<p>First Name: ' + columns[0] + '<br>' +	
+				'Last Name: ' + columns[1] + '<br>' +
+				'Number: ' + columns[2] + '<br>' +  
+				'Size: ' + columns[3] + '<br>'+
+				'</p></li>');	
+			
+		}
+		}
+	})
+	})
+});
+	
+	
+	
+	
--- ASD/App/js/main.js
+++ ASD/App/js/main.js
@@ -1,272 +1,79 @@
-$('#home').live('pageinit',function(){
-	var toChangePage = function (toPageId) {
-        $.mobile.changePage("#" + toPageId , {
-            type:"post",
-            data:$("form").serialize(),
-            reloadPage:true
-        });
-    };
+$(document).ready(function(){
+	var cust = function(){
+		$.ajax({
+			"url":"_view/customers",
+			"dataType":"json",
+			"success":function(data){
+				$.each(data.rows, function(index, customer){
+					var fname = customer.value.First
+					var lname = customer.value.Last
+					$('#customerlist').append(
+						$('<li>').append(
+							$('<a>').attr("href","#")
+								.text(fname+" " + lname)
+						)
+					);
+				});
+				$('#customerlist').listview('refresh');
+				$('#customerButton').hide();
+				}
+			});
+	}
+	$('#customerButton').on('click',cust)
 
-	var crustType = ["Thin","Thick","Pan"];
-	var clearData = $("#clear");
-	clearData.on("click",clearLocal);
+			
+//  project 2 make up
 
-	
-	
-	// Clear Local Storage
-	 var clearLocal = function (){
-        if (localStorage.length === 0){
-            alert("There is nothing to clear");
-        }
-        else{
-            localStorage.clear();
-            alert("All orders have been deleted");
-            window.location.reload();
-            return false;
-        }
-    };
-	
-	
-	
-	// Toggle controls function	
-	function toggleControls(n){
-		switch(n){
-			case "off" :
-				$("#order").hide();
-				$("#clear").show();
-				$("#display").hide();
-				$("#addNew").show();
-				break;
-			case "on":
-				$("#order").show();
-				$("#clear").show();
-				$("#display").show();
-				$("#addNew").hide();
-				$("#items").hide();
-				break;
-			default:
-				return false;
-		}
-	}				
-	
-	var getSelectedRadio = function (){
-		var radio = $(".crust");
-		for (var i=0 ; i < radio.length; i++){
-			if(radio[i].checked) {
-				crustValue = radio[i].value; 
-				console.log(crustValue);
-			}
-		}
+
+	var parseXML = function(xml){
+		console.log(xml)
+		$(xml).find('item').each(function(){
+			var itemList = {};
+			itemList.fname		= $(this).find("fname").text();
+			itemList.lname		= $(this).find("lname").text();
+			itemList.Address	= $(this).find("Address").text();
+			itemList.date		= $(this).find("date").text();
+			itemList.select		= $(this).find("select").text();
+			itemList.crust		= $(this).find("crust").text();
+			itemList.meat		= $(this).find("meat").text();
+			itemList.veggie		= $(this).find("veggie").text();
+			
+			$('#customerlist').append(
+				$('<li>').append(
+					$('<a>').attr("href","#")
+						.text(itemList.fname + " " + itemList.lname+ '<br>' +
+						itemList.Address + '<br>' + itemList.date = '<<br>' + 
+						itemList.select + '<br>' + itemList.crust + '<br>' + 
+						itemList.meat + '<br>' + itemList.veggie)
+				)	
+			)	
+		})
 	}
 	
-	
-
-	// Auto Populate local storage
-	function autoFillData(){
-		for(var n in json){
-			var id = Math.floor(Math.random()*100000001);
-			localStorage.setItem(id, JSON.stringify(json[n]));
-		}
-	}
-	
-	// Customer Data function
-		$("#custData").on('click',function(){
-		$("#customerList").empty();
-		for (var i=0, j = localStorage.length; i<j ; i++){
-			var key = localStorage.key(i);
-			var item = JSON.parse(localStorage.getItem(key));
-			console.log();
-			var makeSubList = $("<li></li>");
-			var makSubLi = $("<h3>" + item.fname[1] + " " + item.lname[1] +"</h3>");
-			var makeLink = $("<a href='#popup' data-role='button' data-rel='dialog' data-transition='pop' id= '" + key +"'>Edit</a>");
-			
-			
-			makeLink.html(makSubLi);
-			makeSubList.append(makeLink).appendTo("#customerList");
-			};
+	$('#xmlData').on("click", function(){
+		$.ajax({
+			type: "GET",
+			url: "XHR/data.xml",
+			dataType: "xml",
+			success: parseXML	
+		});
 	});
-
-		
-	// checkbox saving	
-	/*	function checkTopping(id,type){
-			var checkM = item.meat[1];
-			var checkV = item.veggie[1];
-			var mResult = type.test(checkM);
-			var vResult = type.test(checkV);
-			if(mResult === true || vResult === true){
-			$(id).attr("checked", "checked");
+	
+// csv
+	
+	
+		$.ajax({
+			url:'XHR/data.csv',
+			type:'GET',
+			dataType: 'text',
+			success: function(csvData){
+			console.log(csvData)
 			}
-		}
-		checkTopping("sausage",/Sausage/g);
-		checkTopping("peperoni",/Peperoni/g);
-		checkTopping("cb",/Canadian Bacon/g);
-		checkTopping("beef",/Beef/g);
-		checkTopping("onion" ,/Onion/g);
-		checkTopping("mushroom" ,/Mushroom/g);
-		checkTopping("gp" ,/Green Pepper/g);
-		checkTopping("bo" ,/Black Olives/g);
-		checkTopping("go" ,/Green Olives/g);
-	*/	
-	// Remove initial listener from the imputable
-	
-		
-	
-		$("#sizeData").on('click',function(){
-		$("#sizeList").empty();
-		for (var i=0, j = localStorage.length; i<j ; i++){
-			var key = localStorage.key(i);
-			var item = JSON.parse(localStorage.getItem(key));
-			console.log(item);
-			var makeSubList = $("<li></li>");
-			var makSubLi = $("<h3>" + item.select[0] +" " + item.select[1] +"</h3>");
-			var makeLink = $("<a href='#' id= '" + key +"'>Edit</a>");
-			makeLink.on('click' , function(){
-				console.log("This is my key: " + this.id);
-			});
-			makeLink.html(makSubLi);
-			makeSubList.append(makeLink).appendTo("#sizeList");
-			};
-	});
-		$("#crustData").on('click',function(){
-		$("#curstList").empty();
-		for (var i=0, j = localStorage.length; i<j ; i++){
-			var key = localStorage.key(i);
-			var item = JSON.parse(localStorage.getItem(key));
-			console.log(item);
-			var makeSubList = $("<li></li>");
-			var makSubLi = $("<h3>" + item.crust[1] +"</h3>");
-			var makeLink = $("<a href='#' id= '" + key +"'>Edit</a>");
-			makeLink.on('click' , function(){
-				console.log("This is my key: " + this.id);
-			});
-			makeLink.html(makSubLi);
-			makeSubList.append(makeLink).appendTo("#crustList");
-			};
-	});
-		$("#meatData").on('click',function(){
-		$("#meatList").empty();
-		for (var i=0, j = localStorage.length; i<j ; i++){
-			var key = localStorage.key(i);
-			var item = JSON.parse(localStorage.getItem(key));
-			console.log(item);
-			var makeSubList = $("<li></li>");
-			var makSubLi = $("<h3>" + item.meat[1] +"</h3>");
-			var makeLink = $("<a href='#' id= '" + key +"'>Edit</a>");
-			makeLink.on('click' , function(){
-				console.log("This is my key: " + this.id);
-			});
-			makeLink.html(makSubLi);
-			makeSubList.append(makeLink).appendTo("#meatList");
-			};
-	});
-	$("#veggieData").on('click',function(){
-		$("#veggieList").empty();
-		for (var i=0, j = localStorage.length; i<j ; i++){
-			var key = localStorage.key(i);
-			var item = JSON.parse(localStorage.getItem(key));
-			console.log(item);
-			var makeSubList = $("<li></li>");
-			var makSubLi = $("<h3>" + item.veggie[1] +"</h3>");
-			var makeLink = $("<a href='#' id= '" + key +"'>Edit</a>");
-			makeLink.on('click' , function(){
-				console.log("This is my key: " + this.id);
-			});
-			makeLink.html(makSubLi);
-			makeSubList.append(makeLink).appendTo("#veggieList");
-			};
-	});
-	
-/*	var getOrders = function(){
-		toggleList("off");
-		if (localStorage.length === 0){
-			alert("Loading Data");
-			autoFillData();
-		}
-		var makeDiv = $('#seeData')
-		makeDiv.attr = $("data-role", "content");
-		makeDiv.append("<ul id=" + "datalist" + "></ul>");
-		var makeList = $(#dataList);
-		makeList.attr({dataRole: "listview",dataInset: "true", dataFilter: "true"
-            });
-		for (var i=0, j = localStorage.length; i<j ; i++){
-			var makeLi = $("<li></li>");
-			var linksLi = $("<li></li>");
-			makeList.append(makeLi);
-			var keyVal = localStorage.key(i);
-            var value = localStorage.getItem(keyVal);
-            var obj = JSON.parse(value); 	               
-			var makeSubList = $('<ul></ul>');
-            makeLi.append(makeSubList);
-            for (var j in obj){
-                 var optSubText = obj[j][0]+" "+obj[j][1];                   //separate the label with the value
-                 var makeSubli = $("<li></li>");
-                 console.log(obj);
-                 makeSubli.append(optSubText);
-                 makeSubList.append(makeSubli);
-                 makeSubli.append(linksLi);
-           }
-		 makeItemLinks(localStorage.key(i), linksLi);
- 		    }
-        }
-    };
-
-	
-	var makeItemsLink = function(key, makeSubList){
-		var editLink = $("<a>");
-		editLink.href="#";
-		editLink.key = key;
-		var editText = "Edit Order";
-		editLink.append(editText);
-		makeSubList.appendTo(editLink)
-		var deleteLink = $("<a>");
-		deleteLink.href="#"
-		deleteLink.key = key;
-		var deleteText ="Delete Order";
-		deleteLink.on('click', deleteItem);
-		deleteLink.append(deleteText);
-		makeSubList.appendTo(deleteLink);
-		};
-	*/	
-	var deleteItem = function(){
-		var ask = confirm("Are you sure you want to delete this order");
-		if (ask){
-			localStorage.removeItem(this.key);
-			alert("Order was deleted");
-		}else{
-			alert("Order was not deleted.");
-		}
-	}			
+		})
+});
 	
 	
-// Get Radio function
-	var getRadio = function (){
-		console.log($('input:radio[class=crust]:checked').val());
 	
-	};
-
-// Store Data function
-	var storeData = function(key){
-		if(!key){
-			var id					= Math.floor(Math.random()*100000001);
-		}else{
-			id = key
-		}	
-		
-		var item    			= {};
-			  item.fname		= ["First Name: " , ($("#fname").val())];
-		      item.lname		= ["Last Name: "  , ($("#lname").val())];
-		      item.Address	= ["Address: " , ($("#Address").val())];
-			  item.date		= ["Date: " , ($("#date").val())];
-			  item.number	= ["Number of Pizzas: " , ($("#number").val())];
-			  item.select		= ["Size: " ,($("#size").val())];
-			  item.crust	= ["Crust Type: " , getRadio()];
-		//	  item.meat		= ["Meat Toppings:" , mVal];
-	//		  item.veggie		= ["Veggie Toppings: " , veggieValue];
-		localStorage.setItem(id,  JSON.stringify(item));
-		alert("Order Saved");
-		console.log($("#fname").val());
-	}	
-
-
-
-});	
+	
+	
+	
