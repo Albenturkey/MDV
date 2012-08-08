09c147e435e6a675d68e7111bdea3d64f6c56183
Alan Sherek <albenturkey@fullsail.edu>
2012-08-07 23:00:18 -0500

    Stripped jquery out back to just js

---
+++ AFV/www/js/JSON.js
@@ -0,0 +1,35 @@
+var json = {
+	"order1" : {
+		"fname": ["First Name: " , "Donald"],
+		"lname": ["Last Name: " , "Duck"],
+		"Address": ["Address: " , "Disney Land"],
+		"date": ["Date: " , "02/07/2012"],
+		"number": ["Number of Pizzas: ", "2"],
+		"select" :["Size: ", "Large"],
+		"crust" : ["Crust Type: ", "thick"],
+		"meat" : ["Meat Toppings: " , "Peperoni" ],
+		"veggie" : ["Veggie Toppings: ", "Mushrooms, Onions"]
+	},
+	"order2" : {
+		"fname": ["First Name: " , "Walter "],
+		"lname": ["Last Name: " , "Bishop"],
+		"Address": ["Address: " , "Harvard University University Hall Cambridge, MA 02138-3800"],
+		"date": ["Date: " , "02/07/2012"],
+		"number": ["Number of Pizzas: ", "4"],
+		"select" :["Size: ", "Large"],
+		"crust" : ["Crust Type: ", "pan"],
+		"meat" : ["Meat Toppings: " , "Sausage, Beef" ],
+		"veggie" : ["Veggie Toppings: ", "Black Olives, Onions"]
+	},
+	"order3" : {
+		"fname": ["First Name: " , "Ray"],
+		"lname": ["Last Name: " , "Peloquin"],
+		"Address": ["Address: " , "301 3rd St south, Virginia,Mn"],
+		"date": ["Date: " , "02/28/2012"],
+		"number": ["Number of Pizzas: ", "1"],
+		"select" :["Size: ", "Small"],
+		"crust" : ["Crust Type: ", "thin"],
+		"meat" : ["Meat Toppings: " , "Canadian Bacon" ],
+		"veggie" : ["Veggie Toppings: ", "Mushrooms, Green Pepper"]
+	}
+}
